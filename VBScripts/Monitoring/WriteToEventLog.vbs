ec99072b99350afc5b4f2702b3bbcc730292a741
AlexanderPro <illarionov.pro@gmail.com>
2019-03-24 14:38:18 +0300

    Removed VBScripts to a separate repository.

--- VBScripts/Monitoring/WriteToEventLog.vbs
+++
@@ -1,3 +0,0 @@
-Set shell = WScript.CreateObject("Wscript.Shell")
-shell.LogEvent 0, "Test event."
-WScript.Echo "Script is done!"
--- VBScripts/Sudo/Sudo.cmd
+++
@@ -1,4 +0,0 @@
-@echo Set shell = CreateObject("Shell.Application") > %temp%\sudo.tmp.vbs
-@echo args = Right("%*", (Len("%*") - Len("%1"))) >> %temp%\sudo.tmp.vbs
-@echo shell.ShellExecute "%1", args, "", "runas", 1 >> %temp%\sudo.tmp.vbs
-@start "" /B %temp%\sudo.tmp.vbs %*
