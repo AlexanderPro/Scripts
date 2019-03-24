d48e37361e0e37e4845550ffb5ad80a1074e538b
AlexanderPro <illarionov.pro@gmail.com>
2019-03-24 14:46:59 +0300

    Removed PowerShell scripts to a separate repository.

--- PowerShell/FileSystem/GetFileMD5.ps1
+++
@@ -1,4 +0,0 @@
-$someFilePath = "C:\Temp\1.txt"
-$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-$hash = ($md5.ComputeHash([System.IO.File]::ReadAllBytes($someFilePath)) |  foreach { $_.ToString("X2") }) -join ""
-$hash
